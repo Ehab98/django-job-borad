71a769871a7dcaba2d89615a85a35794c7c81c50
Ehab98 <hoba.reda98@gmail.com>
2021-12-09 03:57:02 +0200

    modify template

--- job/models.py
+++ job/models.py
@@ -13,6 +13,13 @@ JOB_TYPE=(
     ('Part Time','Part Time'),
 
 )
+
+def image_upload(instance,filename):
+    imagename ,extension = filename.split(".")
+    return "jobs/%s.%s"%(instance.id,extension)
+
+
+
 class Job(models.Model): #every class is a table 
     title =models.CharField(max_length=100) #column
     #location
@@ -23,6 +30,7 @@ class Job(models.Model): #every class is a table
     salary =models.IntegerField(default=0)
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
     experience =models.IntegerField(default=1)
+    image=models.ImageField(upload_to=image_upload)
     
     def __str__(self):
         return self.title
--- job/views.py
+++ job/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 
-def job_list(request):
-    pass
+import job
 
+from .models import Job
+
+def job_list(request):
+
+    job_list =Job.objects.all()
+    
+    context={
+        'jobs':job_list
+    }
+    return render(request, 'job/job_list.html',context)
 
 def job_detail(request,id):
-    pass
+    job_detail =Job.objects.get(id=id)
+    context = {
+        'job':job_detail
+    }
+    return render(request,'job/job_detail.html',context)
+    
+    
